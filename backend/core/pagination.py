85305cb421c058c679d3dfc748e9933b39dbff01
fernandaguimaraes089-creator <fernandaguimaraes089@gmail.com>
2026-07-22 13:59:41 -0300

    Add pagination classes

---
+++ backend/core/pagination.py
@@ -0,0 +1,27 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+    
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'page_size': self.page_size,
+            'total_pages': self.page.paginator.num_pages,
+            'results': data
+        })
+
+class LargeResultsSetPagination(PageNumberPagination):
+    page_size = 100
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
+
+class SmallResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 50
